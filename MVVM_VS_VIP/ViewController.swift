c1254e1a9307d07865902ade6f2697b9cb5ae9d8
Papon Supamongkonchai <papon.s@cimbthai.com>
2023-04-07 10:01:31 +0700

    Initial Commit

---
+++ MVVM_VS_VIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_VS_VIP
+//
+//  Created by Papon Supamongkonchai on 7/4/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVM_VS_VIPUITests/MVVM_VS_VIPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVM_VS_VIPUITestsLaunchTests.swift
+//  MVVM_VS_VIPUITests
+//
+//  Created by Papon Supamongkonchai on 7/4/2566 BE.
+//
+
+import XCTest
+
+final class MVVM_VS_VIPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
